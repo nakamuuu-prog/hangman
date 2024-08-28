bdad65e9d69c6d74b18de88127e81aff39b4f14d
nakamuuu-prog <affilimoney2@gmail.com>
2024-08-29 08:32:29 +0900

    10章の1の4終わり

--- src/hangman.ts
+++ src/hangman.ts
@@ -1,1 +1,10 @@
-console.log("Game Start!!");
+// resolveJsonModuleオプションをtrueにするとJSONファイルを読み込めるようになる
+// fsというモジュールを使って読み込むこともできるが、any型に推論されてしまうため注意が必要
+import rawData from "./data/questions.test.json";
+
+interface Question {
+  word: string;
+  hint: string;
+}
+
+const questions: Question[] = rawData;
